84c2d75adf5239d1a94ff0761ba66d33ed0bc337
rachaelvp <rachaelvphillips@gmail.com>
2023-03-08 16:40:48 -0800

    make r code for relevant chapters

--- purl.R
+++ purl.R
@@ -4,7 +4,8 @@ library(stringr)
 
 # get list of chapters for which to create .R files
 chapters <- setdiff(str_subset(dir(), ".Rmd"),
-                    c("index.Rmd", "references.Rmd"))
+                    c("01-introduction.Rmd", "02-preliminaries.Rmd", "03-intro-tlverse.Rmd", 
+                      "04-intro-roadmap.Rmd", "index.Rmd", "references.Rmd"))
 
 # create .R files
 lapply(chapters, function(f) {
